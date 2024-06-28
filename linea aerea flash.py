535f8f9ceb3f7a9d9d26092fdf63c0f0180384e5
Alexander-TorresT <al.torrest@duocuc.cl>
2024-06-28 17:17:37 -0400

    Evaluacion 3
    https://github.com/Alexander-TorresT/Prueba-Alexander/upload/main

---
+++ linea aerea flash.py
@@ -0,0 +1,24 @@
+#Linea aerea Flash
+#Vuelos nacionales Boeing 717
+#Capacidad transportar 198 pasajeros
+#*************************************************
+
+
+
+
+print("comprar pasajes")
+print("mostrar ubicaciones disponibles")
+print("ver listado de pasajeros")
+print("buscar pasajero")
+print("reasignar asiento")
+print("mostrar ganancias totales")
+
+if "Asiento Comun, $60.000" (cantidad 100
+if "Espacio adicional para piernas, $80.000"cantidad 10)
+if "No reclina, $50.000" (cantidad 6)
+if total de asientos (116)
+if total de dinero 7.100.000
+def ("Mostrar ubicaciones disponibles")
+def ("Ver listado de pasajeros")
+def ("Reasignar asiento")
+def ("Mostrar ganancias totales")
